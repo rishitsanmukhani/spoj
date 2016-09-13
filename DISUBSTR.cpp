da0013885de237af39117e0a31ea9564540a4051
Rishit_India <rishitsanmukhani@gmail.com>
2016-09-13 15:09:24 +0530

    Suffix array + LCP

---
+++ DISUBSTR.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define icin(x) scanf("%d",&x)
+typedef long long LL;
+const int maxn=int(1e5)+10;
+
+class SuffixArray{
+public:
+	int n;
+	char S[maxn];
+	int RA[maxn], tempRA[maxn];		// rank array
+	int SA[maxn], tempSA[maxn];		// suffix array
+	int c[maxn];									// counting sort
+	int phi[maxn], PLCP[maxn], LCP[maxn];		// LCP array
+
+	void computeSA(){
+		n = (int)strlen(S);
+		S[n++] = '$';
+
+		for(int i=0;i<n;i++) RA[i] = S[i];
+		for(int i=0;i<n;i++) SA[i] = i;
+		for(int k=1;k<n;k<<=1){
+			countingSort(k);
+			countingSort(0);
+			int r;
+			tempRA[SA[0]] = r = 0;
+			for(int i=1;i<n;i++)
+				tempRA[SA[i]] = ((RA[SA[i]] == RA[SA[i-1]]) && (RA[SA[i]+k] == RA[SA[i-1]+k])) ? r : ++r;
+			for(int i=0;i<n;i++)
+				RA[i] = tempRA[i];
+			if(RA[SA[n-1]] == n-1)
+				break;
+		}
+	}
+	void countingSort(int k){
+		int maxi = max(256, n);
+		fill(c, c + maxi, 0);
+		for(int i=0;i<n;i++)
+			c[i+k<n ? RA[i+k] : 0]++;
+		for(int i=0, sum=0;i<maxi;i++){
+			int t = c[i];
+			c[i] = sum;
+			sum += t;
+		}
+		for(int i=0;i<n;i++)
+			tempSA[c[SA[i]+k<n ? RA[SA[i]+k] : 0]++] = SA[i];
+		for(int i=0;i<n;i++)
+			SA[i] = tempSA[i];
+	}
+	void computeLCP(){
+		phi[SA[0]] = -1;
+		for(int i=1;i<n;i++)
+			phi[SA[i]] = SA[i-1];
+		for(int i=0, l=0;i<n;i++){
+			if(phi[i]==-1){
+				PLCP[i]=0;
+				continue;
+			}
+			while(S[i+l] == S[phi[i]+l])l++;
+			PLCP[i] = l;
+			l = max(l-1, 0);
+		}
+		for(int i=0;i<n;i++)
+			LCP[i] = PLCP[SA[i]];
+	}
+};
+
+int main(){
+	SuffixArray sa;
+	int t;
+	icin(t);
+	while(t--){
+		scanf("%s", sa.S);
+		sa.computeSA();
+		sa.computeLCP();
+		LL res=0;
+		for(int i=1;i<sa.n;i++){
+			res += LL(sa.n-1 - sa.SA[i] - sa.LCP[i]);
+		}
+		printf("%lld\n",res);
+	}
+	return 0;
+}
